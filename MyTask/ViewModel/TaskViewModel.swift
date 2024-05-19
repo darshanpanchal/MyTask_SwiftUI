6bddfe47532759f958b1b33e6a306f85250363cb
darshan <darshanpanchal619@gmail.com>
2024-05-19 21:36:32 +0530

    [IOS] MT1 CRUD Operation using Static data Create Update Delete Operation on MyTask Create New Task using static data Update Existing Task using static data Delete Task using static data

--- MyTask/ViewModel/TaskViewModel.swift
+++ MyTask/ViewModel/TaskViewModel.swift
@@ -11,8 +11,38 @@ final class TaskViewModel: ObservableObject {
     
     @Published var tasks: [Task] = []
     
-    func getTasks(isActive: Bool){
-        tasks = Task.createMockTasks().filter{$0.isActive == isActive}        
-    }
+    private var tempTask = Task.createMockTasks()
     
+    
+    func getTasks(isActive: Bool){
+        tasks = tempTask.filter{$0.isActive == isActive}
+    }
+    func addTask(task: Task) -> Bool{
+        let taskId = Int.random(in: 4...100)
+        let taskToAdd = Task(id: taskId,
+                             name: task.name,
+                             description: task.description,
+                             isActive: true,
+                             finishDate:task.finishDate)
+        tempTask.append(taskToAdd)
+        return true
+    }
+    func updateTask(task: Task)->Bool{
+        if let index = tempTask.firstIndex(where: {$0.id == task.id}) {
+            tempTask[index].name = task.name
+            tempTask[index].description = task.description
+            tempTask[index].isActive = task.isActive
+            tempTask[index].finishDate = task.finishDate
+            return true
+        }
+        return false
+    }
+    func deleteTask(task: Task)->Bool{
+        if let index = tempTask.firstIndex(where: {$0.id == task.id}) {
+            tempTask.remove(at: index)
+            return true
+        }
+        return false
+        
+    }
 }
